6425da2e41cd274905a9a11725e87fb70ba52aaa
Akshay V <83549571+aks-vijay@users.noreply.github.com>
2023-10-03 16:49:33 -0400

    Create constructors.py

---
+++ constructors.py
@@ -0,0 +1,19 @@
+# schema - 
+constructor_schema = "constructorId INT, constructorRef string, name string, nationality string, url string"
+
+constructor = spark.read \
+                .schema(constructor_schema) \
+                .json('/FileStore/tables/constructors.json')
+
+# drop unwanted column
+constructor_dropped_df = constructor.drop(constructor["url"])
+
+# rename column
+from pyspark.sql.functions import current_timestamp
+
+constructor_final_df = constructor_dropped_df \
+                        .withColumnRenamed("constructorId", "constructor_id") \
+                        .withColumnRenamed("constructorRef", "constructor_ref") \
+                        .withColumn("ingestion_date", current_timestamp())
+
+constructor_final_df.write.mode("overwrite").parquet('/dbfs/FileStore/parquet_output/processed/constructors')
