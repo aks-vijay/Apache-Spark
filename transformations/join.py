3006d9ad1433d63cb4d667239ea80d937c2f9014
Akshay V <83549571+aks-vijay@users.noreply.github.com>
2023-10-04 10:35:39 -0400

    Update and rename innerjoin.py to join.py

---
+++ transformations/join.py
@@ -0,0 +1,26 @@
+# fetch the processed folder path
+%run "../configuration/configuration"
+
+# import from ingested parquet files
+df_circuits = spark.read.parquet(f"{processed_folder_path}/circuits").where("circuit_id <= 70")
+df_races = spark.read.parquet(f"{processed_folder_path}/races").where("race_year == 2019")
+
+# inner join both the dataframes
+df_joined = df_circuits.join(df_races, df_circuits.circuit_id == df_races.circuit_id, "inner") \
+    .select(
+            df_circuits.name.alias("circuit_name"), 
+            df_circuits.location, 
+            df_circuits.country, 
+            df_races.name.alias("race_name"), 
+            df_races.round
+            )
+
+# left join both the dataframes
+df_joined = df_circuits.join(df_races, df_circuits.circuit_id == df_races.circuit_id, "left") \
+    .select(
+            df_circuits.name.alias("circuit_name"), 
+            df_circuits.location, 
+            df_circuits.country, 
+            df_races.name.alias("race_name"), 
+            df_races.round
+            )
