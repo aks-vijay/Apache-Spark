6dcbba5ff02bcad53b4f47a178694d10140c2387
Akshay V <83549571+aks-vijay@users.noreply.github.com>
2023-10-04 08:39:47 -0400

    Create configurations.py

---
+++ configurations.py
@@ -0,0 +1,3 @@
+raw_folder_path = 'dbfs:/FileStore/tables'
+processed_folder_path = '/dbfs/FileStore/parquet_output/processed'
+presentation_folder_path = '/dbfs/FileStore/parquet_output/presentation'
