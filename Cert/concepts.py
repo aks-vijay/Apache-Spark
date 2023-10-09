5685f5e3798fdaa9c103ac20e5788ea7f5eb2399
Akshay V <83549571+aks-vijay@users.noreply.github.com>
2023-10-09 18:35:41 -0400

    Update concepts.py

--- Cert/concepts.py
+++ Cert/concepts.py
@@ -8,3 +8,31 @@ Coalesce
 - ONLY DECREASES
 - It completely eliminates the partition
 - Eliminates full shuffle and executes as narrow transformation
+
+# to check paritions on the df
+print(df.rdd.getNumPartitions())
+
+# coalesce
+df.coalesce(1) \
+    .write.mode("overwrite").format("csv").save(
+        f"dbfs:/FileStore/{user}/courses/")
+
+df.coalesce(1) \
+    .write.mode("overwrite") \
+    .csv(
+        f"dbfs:/FileStore/{user}/courses/",
+        compression="gzip"
+        )
+# reparition
+df.repartition(1) \
+    .write.mode("overwrite").format("csv").save(
+        f"dbfs:/FileStore/{user}/courses/")
+
+df.repartition(1) \
+    .write.mode("overwrite") \
+    .csv(
+        f"dbfs:/FileStore/{user}/courses/",
+        compression="gzip"
+        )
+
+print(df.rdd.getNumPartitions())
